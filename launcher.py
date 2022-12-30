494c7a35d126f15d43ede55c61214d050a1f4d3c
pl0ox <84785786+pl0ox@users.noreply.github.com>
2022-12-30 16:53:45 +0200

    Play Music in Your Discord Server with Simple Commands.
    Introducing the Discord Music Bot - a powerful, easy-to-use music player for your Discord server. With just a few simple commands, you can play music from popular streaming platforms like Spotify and YouTube right in your Discord voice channel. Whether you want to listen to your favorite playlist, or just need some background music while you chat, this bot has you covered.
    
    Features:
    
    Play music from YouTube
    Create and manage playlists
    Adjust volume and skip tracks
    Works with any voice channel
    Getting started is easy - just invite the bot to your server and use the !help command to see all the available commands. Try it out today and bring the party to your Discord server!

---
+++ launcher.py
@@ -0,0 +1,13 @@
+from bot import MusicBot
+
+
+def main():
+    bot = MusicBot()
+    bot.run()
+
+
+if __name__ == "__main__":
+    main()
+
+# java -jar Lavalink.jar 
+# cd C:\Program Ficles\Java\jdk-11.0.11\bin
